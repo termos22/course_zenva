100bc939b7fe58ebc23ce536713caba4a8422874
Jakub Przybysz <termos22@gmail.com>
2025-02-26 16:38:28 +0100

    finshed condition if lesson

--- 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/2.18_ex_conditions_if_else_elif2.py
+++ 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/2.18_ex_conditions_if_else_elif2.py
@@ -1,20 +1,23 @@
 a = 5
-b = 5
+b = 3
 
 if a == b:
   print("a is equal to b")
 
-if a > b:
+elif a > b:
   print("a is greater than b")
 
-if a < b:
+elif a < b:
   print("a is less than b")
   
-if a != b:
+elif a != b:
   print("a doesn't equal b")
   
-if a >= b:
+elif a >= b:
   print("a is greater than or equals to b")
 
-if a <= b:
+elif a <= b:
   print("a is less than or equals to b")
+
+else:
+  print('none of the above')
