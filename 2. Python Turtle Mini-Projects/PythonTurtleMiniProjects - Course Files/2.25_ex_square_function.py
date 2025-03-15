11d643c150e56f22948635f94b60d794b3f6815d
Jakub Przybysz <termos22@gmail.com>
2025-03-15 20:45:37 +0100

    create new file with squares

--- 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/2.25_ex_square_function.py
+++ 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/2.25_ex_square_function.py
@@ -1,9 +1,11 @@
 from turtle import *
 
-def draw_square(size, x_cord = 0, y_cord = 0):
+def draw_square(size, tlo, x_cord = 0, y_cord = 0):
     penup()
     goto(x_cord, y_cord)
     pendown()
+    fillcolor(tlo)
+    begin_fill()
     forward(size)
     left(90)
     forward(size)
@@ -11,12 +13,13 @@ def draw_square(size, x_cord = 0, y_cord = 0):
     forward(size)
     left(90)
     forward(size)
+    end_fill()
 
-draw_square(50)
-draw_square(30, 100, 100)
-draw_square(30, -50, 100)
-draw_square(30, -50, -50)
-draw_square(30, 100, -50)
+draw_square(50, "red")
+draw_square(30, "blue", 100, 100)
+draw_square(30, "yellow", -50, 100)
+draw_square(30, "green", -50, -50)
+draw_square(30, "black", 100, -50)
 
 
 
