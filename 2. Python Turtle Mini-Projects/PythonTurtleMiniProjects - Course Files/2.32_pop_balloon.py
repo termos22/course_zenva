1e0eb4580ec4c18ab3421cc068d00624862b501f
Jakub Przybysz <termos22@gmail.com>
2025-03-20 18:10:56 +0100

    balloon poper skonczony, dodane komentarze

--- 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/2.32_pop_balloon.py
+++ 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/2.32_pop_balloon.py
@@ -1,30 +1,28 @@
 from turtle import *
 
-diameter = 40
-pop_diameter = 100
+diameter = 40 # definiuje wielkość początkowego balonu
+pop_diameter = 100 # definiuje maksymalną wielkość balonu po osiągnięciu której następuje wybuch 
 
-def draw_blloon():
+def draw_blloon(): # definicja funkcji rysowania poczatkowego balonu
     color("red")
     # begin_fill()
     # circle(diameter)
     # end_fill()
-    dot(diameter)
+    dot(diameter) # zamiast circle, rysuje wypełniony kolorem okrąg o zadanej wielkości
 
-def inflate_balloon():
-    global diameter
+def inflate_balloon(): # definicja funkcji powiększania balonu
+    global diameter # definiuje zmienną jako globalną żeby funkcjie jej używające mogły ją zapisywać
     diameter = diameter + 10
     draw_blloon()
 
-    if diameter >= pop_diameter:
-        clear()
-        diameter = 40
-        write("POP!")
+    if diameter >= pop_diameter: # porównuje wielkość balonu z zadaną wartością graniczną
+        clear() # czyści ekran
+        diameter = 40 # przywraca początkową wartość wielkości balonu
+        write("POP!") # wyświetla napis na ekranie 
 
-draw_blloon()
-inflate_balloon()
+draw_blloon() # rysuje początkowy balon na ekranie
 
-
-#onkey(inflate_balloon, "Up")
-#listen()
+onkey(inflate_balloon, "Up") # czyta wejścia i uruchamia funkcję, w tym przypadku powiększa balon za pomocą klawisza strzałki w górę
+listen() # utrzymuje program w trybie odczytywania wejść
 
 done()
