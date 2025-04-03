a68e3aa6d70cd4fb45f1b5456234889ef8300ec6
Jakub Przybysz <termos22@gmail.com>
2025-04-04 00:52:54 +0200

    soome mod of ballon popper

---
+++ 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/2.39_pop_balloon_mod.py
@@ -0,0 +1,36 @@
+from turtle import *
+
+diameter = 40 # definiuje wielkość początkowego balonu
+pop_diameter = 100 # definiuje maksymalną wielkość balonu po osiągnięciu której następuje wybuch 
+
+def draw_balloon(): # definicja funkcji rysowania poczatkowego balonu
+    color("red")
+    # begin_fill()
+    # circle(diameter)
+    # end_fill()
+    dot(diameter) # zamiast circle, rysuje wypełniony kolorem okrąg o zadanej wielkości
+
+def inflate_balloon(): # definicja funkcji powiększania balonu
+    global diameter # definiuje zmienną jako globalną żeby funkcjie jej używające mogły ją zapisywać
+    diameter = diameter + 10
+    draw_balloon()
+
+    if diameter >= pop_diameter: # porównuje wielkość balonu z zadaną wartością graniczną
+        clear() # czyści ekran
+        diameter = 40 # przywraca początkową wartość wielkości balonu
+        write("POP!") # wyświetla napis na ekranie 
+
+def deflate_balloon():
+    global diameter
+    diameter = diameter - 10
+    clear()
+    draw_balloon()
+
+
+draw_balloon() # rysuje początkowy balon na ekranie
+
+onkey(inflate_balloon, "Up") # czyta wejścia i uruchamia funkcję, w tym przypadku powiększa balon za pomocą klawisza strzałki w górę
+onkey(deflate_balloon, "Down")
+listen() # utrzymuje program w trybie odczytywania wejść
+
+done()
