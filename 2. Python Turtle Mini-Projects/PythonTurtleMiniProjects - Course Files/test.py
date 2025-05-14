256d929c7d25061477006f9ae253092c85e50dbc
Jakub Przybysz <termos22@gmail.com>
2025-05-14 18:42:50 +0200

    some tests in external file with random colors

--- 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/test.py
+++ 2. Python Turtle Mini-Projects/PythonTurtleMiniProjects - Course Files/test.py
@@ -1,11 +1,17 @@
-import random
+from turtle import *
+from random import randint
 
-def losowy_kolor_rgb():
-  """Generuje losowy kolor w formacie RGB (tuple)."""
-  red = random.randint(0, 255)
-  green = random.randint(0, 255)
-  blue = random.randint(0, 255)
-  return (red, green, blue)
+def losowy_kolor():
+    red = randint(0, 255)
+    green = randint(0, 255)
+    blue = randint(0, 255)
+    return (red / 255.0, green / 255.0, blue / 255.0)
 
-losowy_kolor = losowy_kolor_rgb()
-print(f"Losowy kolor RGB: {losowy_kolor}")
+#ekran = turtle.Screen()
+
+
+# Wywołaj funkcję losowy_kolor() i użyj wyniku jako koloru kropki
+kolor = losowy_kolor()
+dot(50, kolor) # 50 to rozmiar kropki
+
+done()
